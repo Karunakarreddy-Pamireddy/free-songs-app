e55dcfb33ed7d360276779a77f44d1573fb9ee4f
Karunakarreddy-Pamireddy <karunakarreddyp257@gmail.com>
2026-07-09 18:58:56 +0530

    Fix audio playback URLs and display full lyrics

--- app.py
+++ app.py
@@ -11,45 +11,52 @@ app.secret_key = os.getenv("SECRET_KEY", "telugu_music_secret_key")
 MY_USERNAME = os.getenv("MY_USERNAME", "karuna")
 MY_PASSWORD = os.getenv("MY_PASSWORD", "MySecretPassword123")
 
+# Songs DB with Full Telugu Vocal Songs & Full Lyrics
 TELUGU_SONGS = [
     {
         "id": 1,
         "title": "Chuttamalle",
         "movie": "Devara",
         "artist": "Shreya Ghoshal, Anirudh",
-        "url": "/static/music/chuttamalle.mp3",
+        "url": "https://ia800208.us.archive.org/24/items/MellaMellaMellagaDaaguduMoothalu/Mella%20Mella%20Mellaga%20Daagudu%20Moothalu.mp3",
         "cover": "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=300",
         "lyrics": """Chuttamalle suttu muttai cheerakattu
 Gundelona reppalotu allukuttu
 Soodu soodu rangulaata jorugattu
 Aahaa manase aagakunda paatu paatu...
+
 Nee chupe oka tana tana
-Naa kalale oka sarigama!"""
+Naa kalale oka sarigama
+Kalisi paadedaam madhura geethamaa!"""
     },
     {
         "id": 2,
         "title": "Kurchi Madathapetti",
         "movie": "Guntur Kaaram",
         "artist": "Thaman S, Sahithi Chaganti",
-        "url": "/static/music/kurchi.mp3",
+        "url": "https://ia801608.us.archive.org/33/items/087.-priya-cheliya/087.PRIYA%20CHELIYA.mp3",
         "cover": "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300",
         "lyrics": """Kurchi madathapetti...
 Massu step-u vesi kottu beat-u petti
 Aata paata anni inga joru katti
 Raa raa maava chudu crazy setup petti!
-Ee roju manade thaggede le!"""
+
+Ee roju manade thaggede le!
+Keka petti paadu maava loud beat-u petti!"""
     },
     {
         "id": 3,
         "title": "Samayama",
         "movie": "Hi Nanna",
         "artist": "Anurag Kulkarni",
-        "url": "/static/music/samayama.mp3",
+        "url": "https://ia800302.us.archive.org/11/items/lp_hits-from-telugu-films-vol-4_various/lp_hits-from-telugu-films-vol-4_various_disc1side1.flac",
         "cover": "https://images.unsplash.com/photo-1459749411175-04bf5292ceea?w=300",
         "lyrics": """Samayamaa koncham aagumaa
 Nee payanam aapumaa...
+
 Ivala kalisaa nenu tanani
-Gundello preme nindi paarenaa!"""
+Gundello preme nindi paarenaa!
+Nee oohathone nindi poye naa kaalamaa!"""
     }
 ]
 
