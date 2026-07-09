6d9d3e5efdb914d315b75f75e1a2d665cf2884a4
Karunakarreddy-Pamireddy <karunakarreddyp257@gmail.com>
2026-07-09 19:01:35 +0530

    Remove lyrics section and update song cards

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app.secret_key = os.getenv("SECRET_KEY", "telugu_music_secret_key")
 MY_USERNAME = os.getenv("MY_USERNAME", "karuna")
 MY_PASSWORD = os.getenv("MY_PASSWORD", "MySecretPassword123")
 
-# Songs DB with Full Telugu Vocal Songs & Full Lyrics
 TELUGU_SONGS = [
     {
         "id": 1,
@@ -19,15 +18,7 @@ TELUGU_SONGS = [
         "movie": "Devara",
         "artist": "Shreya Ghoshal, Anirudh",
         "url": "https://ia800208.us.archive.org/24/items/MellaMellaMellagaDaaguduMoothalu/Mella%20Mella%20Mellaga%20Daagudu%20Moothalu.mp3",
-        "cover": "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=300",
-        "lyrics": """Chuttamalle suttu muttai cheerakattu
-Gundelona reppalotu allukuttu
-Soodu soodu rangulaata jorugattu
-Aahaa manase aagakunda paatu paatu...
-
-Nee chupe oka tana tana
-Naa kalale oka sarigama
-Kalisi paadedaam madhura geethamaa!"""
+        "cover": "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=300"
     },
     {
         "id": 2,
@@ -35,14 +26,7 @@ Kalisi paadedaam madhura geethamaa!"""
         "movie": "Guntur Kaaram",
         "artist": "Thaman S, Sahithi Chaganti",
         "url": "https://ia801608.us.archive.org/33/items/087.-priya-cheliya/087.PRIYA%20CHELIYA.mp3",
-        "cover": "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300",
-        "lyrics": """Kurchi madathapetti...
-Massu step-u vesi kottu beat-u petti
-Aata paata anni inga joru katti
-Raa raa maava chudu crazy setup petti!
-
-Ee roju manade thaggede le!
-Keka petti paadu maava loud beat-u petti!"""
+        "cover": "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300"
     },
     {
         "id": 3,
@@ -50,13 +34,23 @@ Keka petti paadu maava loud beat-u petti!"""
         "movie": "Hi Nanna",
         "artist": "Anurag Kulkarni",
         "url": "https://ia800302.us.archive.org/11/items/lp_hits-from-telugu-films-vol-4_various/lp_hits-from-telugu-films-vol-4_various_disc1side1.flac",
-        "cover": "https://images.unsplash.com/photo-1459749411175-04bf5292ceea?w=300",
-        "lyrics": """Samayamaa koncham aagumaa
-Nee payanam aapumaa...
-
-Ivala kalisaa nenu tanani
-Gundello preme nindi paarenaa!
-Nee oohathone nindi poye naa kaalamaa!"""
+        "cover": "https://images.unsplash.com/photo-1459749411175-04bf5292ceea?w=300"
+    },
+    {
+        "id": 4,
+        "title": "Fear Song",
+        "movie": "Devara",
+        "artist": "Anirudh Ravichander",
+        "url": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
+        "cover": "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=300"
+    },
+    {
+        "id": 5,
+        "title": "Pushpa Pushpa",
+        "movie": "Pushpa 2 The Rule",
+        "artist": "Nakash Aziz, DSP",
+        "url": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
+        "cover": "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300"
     }
 ]
 
